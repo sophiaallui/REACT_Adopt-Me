328f6b8c7af33c3b163114c6f2d530a4697d122c
Sophia Lui <solui@ucsd.edu>
2021-06-21 07:46:02 -0700

    update from finishing effect part

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,52 +1,27 @@
-import React, { useState } from 'react'
-import { ANIMALS } from '@frontendmasters/pet'; 
+import React, { useState, useEffect } from 'react'
+import pet, { ANIMALS } from '@frontendmasters/pet'; 
 import useDropdown from './useDropdown';
 // search box 
 const SearchParams = () => { 
     //const location = "Seattle, WA"; 
 
     const [location, setLocation] = useState("Seattle, WA");
-    //const [animal, setAnimal] = useState("dog");
-    //const [breed, setBreed] = useState("");
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); 
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds); 
-/*
-    return (
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds); 
 
-        <div className="search-params">
-            <form>
-                <label htmlFor="location"> 
-                    Location 
-                    <input id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)}/>
-                </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select id="animal" value={animal} onChange={e=>setAnimal(e.target.value)} onBlur={e => setAnimal(e.target.value)}>
-                        <option>All</option>
-                        {ANIMALS.map(animal => (<option key={animal} value={animal}>{animal}</option>))}
-                    </select>
-                </label>
+   useEffect(()=> {
+       //pet.breeds("dogs").then(console.log, console.error );
+       setBreeds([]); 
+       setBreed(""); 
 
-                <label htmlFor="breed">
+       // Call to the api
+        pet.breeds(animal).then(({breeds: apiBreeds}) => { 
+            const breedStrings = apiBreeds.map(({name}) => name); 
+            setBreeds(breedStrings); 
+        },console.error)
+   }, [animal, setBreed, setBreeds]);   
 
-                    Breed
-                    <select id="breed" value={breed} onChange={e=>setBreed(e.target.value)} onBlur={e=>setBreed(e.target.value)} disabled={breeds.length === 0}>
-
-                        <option>All</option>
-                        {breeds.map(breedString => <option key={breedString} value={breedString}>{breedString}</option>)}
-
-                    </select>
-
-
-                </label>
-                <button>Submit</button>
-            </form>
-            
-        </div>
- 
-    )
-    */ 
     return (
 
         <div className="search-params">
