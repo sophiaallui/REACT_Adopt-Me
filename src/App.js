fba07b3ff440a2ed3e1021dac624a9735c1af74a
Sophia Lui <solui@ucsd.edu>
2021-06-22 10:34:14 -0700

    updated to use context in search button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Link, Router } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from './ThemeContext';
 
 const App = () => {
+  const themeHook = useState('darkblue');
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router> 
-        <SearchParams path="/" /> 
-        <Details path="/details/:id"/>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router> 
+          <SearchParams path="/" /> 
+          <Details path="/details/:id"/>
+        </Router>
+      </div>
+
+    </ThemeContext.Provider>
+    
 
   );
 };
