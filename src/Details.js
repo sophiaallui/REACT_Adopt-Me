32c480b2ab90cae4133c053fe3207c3865fce385
Sophia Lui <solui@ucsd.edu>
2021-06-22 09:26:44 -0700

    fixed Detail label

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,60 @@
 import React from 'react'; 
+import pet from '@frontendmasters/pet'
+import Carousel from './Carousel'; 
 
-const Detailts = (props) => { 
-    //return <h1>hi lol</h1>
-    return(
+class Details extends React.Component { 
+   /* constructor(props){ 
+        super(props); 
+        this.state = { 
+            loading: true
+        }; 
+    }*/ 
+    state = {loading:true}; 
 
-        <pre>
-            <code>{JSON.stringify(props,null, 4)}</code>
-        </pre>
+    componentDidMount(){ 
+        pet.animal(this.props.id).then(({animal}) => {
+            this.setState({
+                name: animal.name,
+                animal: animal.type, 
+                location: `
+                ${animal.contact.address.city}, 
+                ${animal.contact.address.state}
+                `, 
+                description: animal.description, 
+                media: animal.photos, 
+                breed: animal.breeds.primary, 
+                loading: false
+            });
+        },console.error); 
+    } 
+    render () { 
 
+        console.log("ENTERED TO DETAILS.JS")
 
-    )
-}; 
-export default Detailts; 
+        console.log("Check state in render details.js", this.state.loading)
+        if(this.state.loading){ 
+            console.log("Check state in render details.js", this.state.loading)
+            return <h1>Loading...</h1>
+            
+        }
+
+        // destruction 
+        // pulling from this.state
+        const {animal, breed, location, description, name, media} = this.state;
+        return(
+            <div className="details">
+                <Carousel media={media}/>
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+
+            </div>
+        );
+
+    }
+}
+ 
+export default Details; 
